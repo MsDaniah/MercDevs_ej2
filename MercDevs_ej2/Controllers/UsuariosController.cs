eea2072edad42b9e64b022c3726b4e1a7234c4e5
MsDaniah <daniela.castillo.mamani@cftmail.cl>
2024-06-19 23:32:11 -0400

    Agregar archivos de proyecto.

---
+++ MercDevs_ej2/Controllers/UsuariosController.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MercDevs_ej2.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace MercDevs_ej2.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly MercydevsEjercicio2Context _context;
+      //  private readonly IPasswordHasher<Usuario> _passwordHasher;
+        
+        public UsuariosController(MercydevsEjercicio2Context context)
+        {
+            _context = context;
+           // _passwordHasher = passwordHasher;
+        }
+
+        // GET: Usuarios
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Usuarios.ToListAsync());
+        }
+
+        // GET: Usuarios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.Usuarios
+                .FirstOrDefaultAsync(m => m.IdUsuario == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Usuarios/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdUsuario,Nombre,Apellido,Correo,Password")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                // Hash the password before saving
+               // usuario.Password = _passwordHasher.HashPassword(usuario, usuario.Password);
+
+                _context.Add(usuario);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.Usuarios.FindAsync(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+            return View(usuario);
+        }
+
+        // POST: Usuarios/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdUsuario,Nombre,Apellido,Correo,Password")] Usuario usuario)
+        {
+            if (id != usuario.IdUsuario)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    // Retrieve the existing user from the database
+                    var existingUser = await _context.Usuarios.FindAsync(id);
+                    if (existingUser == null)
+                    {
+                        return NotFound();
+                    }
+
+                    // If the password has been changed, hash the new password
+                  //  if (!string.IsNullOrEmpty(usuario.Password))
+                  //  {
+                   //     existingUser.Password = _passwordHasher.HashPassword(usuario, usuario.Password);
+                  //  }
+
+                    // Update other fields
+                    existingUser.Nombre = usuario.Nombre;
+                    existingUser.Apellido = usuario.Apellido;
+                    existingUser.Correo = usuario.Correo;
+
+                    _context.Update(existingUser);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UsuarioExists(usuario.IdUsuario))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.Usuarios
+                .FirstOrDefaultAsync(m => m.IdUsuario == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+
+        // POST: Usuarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+            if (usuario != null)
+            {
+                _context.Usuarios.Remove(usuario);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UsuarioExists(int id)
+        {
+            return _context.Usuarios.Any(e => e.IdUsuario == id);
+        }
+    }
+}
